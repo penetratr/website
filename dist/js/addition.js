5dc3eb66f84f8746b61db21e87db06fb3bbdab68
Suienish <aldaniw9854@gmail.com>
2022-11-09 15:28:01 +0600

    chat

---
+++ dist/js/addition.js
@@ -0,0 +1,9 @@
+
+open_chat = document.querySelector(".okno").addEventListener("click", function(){
+    document.querySelector(".chatopen").style.visibility = "hidden";
+    document.querySelector(".chat_open_active").style.visibility = "visible";
+})
+
+
+
+
