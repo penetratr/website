b23f1d1fea38852658dc7fe5e7a69054f1ef4934
Suienish <aldaniw9854@gmail.com>
2022-11-13 22:14:35 +0600

    download button

--- dist/js/addition.js
+++ dist/js/addition.js
@@ -4,6 +4,19 @@ document.querySelector(".okno_chata").addEventListener("click", function(){
     document.querySelector(".chat_open_active").style.visibility = "visible";
 })
 
+document.querySelector(".download").addEventListener("click", function(){
+    if(document.querySelector(".download_two_button").style.visibility == "visible"){
+        document.querySelector(".download_two_button").style.visibility = "hidden";
+    }  
+    else{
+        document.querySelector(".download_two_button").style.visibility = "visible";
+    }
+})
+
+
+
+
+
 
 
 
